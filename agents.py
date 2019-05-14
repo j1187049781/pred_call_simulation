8d0e01612f2a9c57f720a26b1b767078d526c4f8
ubuntu <1187049781@qq.com>
2019-05-14 18:11:47 +0800

    fix bug: test switcher

--- agents.py
+++ agents.py
@@ -4,7 +4,8 @@ class Agent(simpy.Resource):
     def __init__(self,env,capacity):
         super().__init__(env,capacity)
 
-
+    def get_usage_precent(self):
+        return self.count/self.capacity
 
 
 
